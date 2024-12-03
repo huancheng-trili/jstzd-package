1a48362d005fba83c06672196889ae9723a6d5a2
Huan-Cheng Chang <huancheng.chang@trili.tech>
2024-12-03 12:14:08 +0000

    update

--- install.js
+++ install.js
@@ -42,32 +42,6 @@ function makeRequest(url) {
   });
 }
 
-function extractFileFromTarball(tarballBuffer, filepath) {
-  // Tar archives are organized in 512 byte blocks.
-  // Blocks can either be header blocks or data blocks.
-  // Header blocks contain file names of the archive in the first 100 bytes, terminated by a null byte.
-  // The size of a file is contained in bytes 124-135 of a header block.
-  // The following blocks will be data blocks containing the file.
-  let offset = 0;
-  while (offset < tarballBuffer.length) {
-    const header = tarballBuffer.subarray(offset, offset + 512);
-    offset += 512;
-
-    const fileName = header.toString("utf-8", 0, 100).replace(/\0.*/g, "");
-    const fileSize = parseInt(
-      header.toString("utf-8", 124, 136).replace(/\0.*/g, ""),
-      8
-    );
-
-    if (fileName === filepath) {
-      return tarballBuffer.subarray(offset, offset + fileSize);
-    }
-
-    // Clamp offset to the uppoer multiple of 512
-    offset = (offset + fileSize + 511) & ~511;
-  }
-}
-
 async function downloadBinaryFromNpm() {
   // Download the tarball of the right binary distribution package
   const downloadBuffer = await makeRequest(
@@ -94,14 +68,4 @@ function isPlatformSpecificPackageInstalled() {
   }
 }
 
-// Skip downloading the binary if it was already installed via optionalDependencies
-if (!isPlatformSpecificPackageInstalled()) {
-  console.log(
-    "Platform specific package not found. Will manually download binary."
-  );
-  downloadBinaryFromNpm();
-} else {
-  console.log(
-    "Platform specific package already installed. Will fall back to manually downloading binary."
-  );
-}
+downloadBinaryFromNpm();
