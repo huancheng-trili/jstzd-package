5b5170cd4bb3a023bf3f8f31769b7093d9b89530
Huan-Cheng Chang <huancheng.chang@trili.tech>
2024-12-03 14:29:28 +0000

    update

--- install.js
+++ install.js
@@ -1,4 +1,5 @@
 #!/usr/bin/env node
+/*
 const fs = require("fs");
 const path = require("path");
 const https = require("https");
@@ -68,3 +69,6 @@ function isPlatformSpecificPackageInstalled() {
 }
 
 downloadBinaryFromNpm();
+*/
+
+console.log('hi');
