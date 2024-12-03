f67f4ec89fffc0fa174d0c7c06b86c97872fca5d
Huan-Cheng Chang <huancheng.chang@trili.tech>
2024-12-03 11:57:22 +0000

    update

---
+++ index.js
@@ -0,0 +1,20 @@
+const path = require("path");
+const childProcess = require("child_process");
+
+// Windows binaries end with .exe so we need to special case them.
+const binaryName = "jstzd";
+
+function getBinaryPath() {
+  try {
+    // Resolving will fail if the optionalDependency was not installed
+    return require.resolve(`bin/${binaryName}`);
+  } catch (e) {
+    return path.join(__dirname, "..", binaryName);
+  }
+}
+
+module.exports.runBinary = function (...args) {
+  childProcess.execFileSync(getBinaryPath(), args, {
+    stdio: "inherit",
+  });
+};
